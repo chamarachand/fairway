68218db67074217f2ff5d4486ad6836d5f512440
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2026-09-15 17:54:37 +0530

    chore: Initialise project

---
+++ android/app/src/main/kotlin/com/example/fairway/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fairway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fairway',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
