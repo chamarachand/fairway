8d18c3084e84a66370c86721699adf73766039c2
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2026-09-15 19:13:49 +0530

    feat: Implement API service and error handling

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+abstract class ApiConstants {
+  static const String baseUrl = 'https://dummyjson.com';
+
+  static const String products = '$baseUrl/products';
+}
---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:fairway/core/errors/exceptions.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  Future<dynamic> get(String url) async {
+    final uri = Uri.parse(url);
+
+    try {
+      final response = await http.get(uri).timeout(const Duration(seconds: 10));
+      final statusCode = response.statusCode;
+
+      if (statusCode >= 200 && statusCode < 300) {
+        return jsonDecode(response.body);
+      } else {
+        debugPrint('Server error: ${response.statusCode}');
+        throw const ServerException();
+      }
+    } on SocketException {
+      throw const NetworkException();
+    } on TimeoutException {
+      throw const NetworkException(
+        'Connection timed out. Please check your internet speed',
+      );
+    } on AppException {
+      rethrow;
+    } catch (e) {
+      throw const UnknownException();
+    }
+  }
+}
