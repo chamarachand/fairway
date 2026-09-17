8a735b464e36d5e853f3e7844da6414b8cc81ba5
Chamara Chandrasekara <lasithachandrasekara@gmail.com>
2026-09-17 10:10:51 +0530

    fix: move multi bloc provider above router

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,23 +1,16 @@
-import 'package:fairway/core/di/injection.dart';
-import 'package:fairway/features/products/presentation/cubit/category_cubit.dart';
-import 'package:fairway/features/products/presentation/cubit/product_cubit.dart';
+import 'package:fairway/features/products/presentation/pages/product_details_page.dart';
 import 'package:fairway/features/products/presentation/pages/product_list_screen.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
+    GoRoute(path: '/', builder: (context, state) => const ProductListScreen()),
     GoRoute(
-      path: '/',
+      path: '/product/:id',
       builder: (context, state) {
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (_) => getIt<CategoryCubit>()),
-            BlocProvider(create: (_) => getIt<ProductCubit>()),
-          ],
-          child: const ProductListScreen(),
-        );
+        final id = state.pathParameters['id']!;
+        return ProductDetailsPage(productId: id);
       },
     ),
   ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:fairway/core/di/injection.dart';
 import 'package:fairway/core/routing/router.dart';
+import 'package:fairway/features/products/presentation/cubit/category_cubit.dart';
+import 'package:fairway/features/products/presentation/cubit/product_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   setUpDependencies();
@@ -12,13 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Fairway',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<CategoryCubit>()),
+        BlocProvider(create: (_) => getIt<ProductCubit>()),
+      ],
+      child: MaterialApp.router(
+        title: 'Fairway',
+        theme: ThemeData(
+          colorScheme: .fromSeed(seedColor: Colors.teal),
+          useMaterial3: true,
+        ),
+        routerConfig: router,
       ),
-      routerConfig: router,
     );
   }
 }
